8acbaf9614a4d7d830bb4f6a36c0c719ea3b7320
lihp <465371143@qq.com>
2022-01-06 15:34:31 +0800

    添加student成员变量age

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -1,6 +1,9 @@
 package com.bjpowernode.domain;
 
+import javax.persistence.criteria.CriteriaBuilder;
+
 public class Student {
     private String name;
     private String address;
+    private Integer age;
 }
